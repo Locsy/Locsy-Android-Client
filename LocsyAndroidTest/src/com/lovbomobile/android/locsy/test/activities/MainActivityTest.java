45d12a9bb3a977acc989ad9c18e7b69e7adf2994
Sven Schindler <sschindl@uni-potsdam.de>
2013-07-25 23:20:56 +0200

    adds license headers

--- LocsyAndroidTest/src/com/lovbomobile/android/locsy/test/activities/MainActivityTest.java
+++ LocsyAndroidTest/src/com/lovbomobile/android/locsy/test/activities/MainActivityTest.java
@@ -1,3 +1,25 @@
+/**
+ Copyright (c) 2013 Sven Schindler
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
 package com.lovbomobile.android.locsy.test.activities;
 
 import android.location.Location;
@@ -8,16 +30,6 @@ import com.lovbomobile.android.locsy.R;
 import com.lovbomobile.android.locsy.activities.LocsyApplication;
 import com.lovbomobile.android.locsy.activities.MainActivity;
 
-/**
- * This is a simple framework for a test of an Application.  See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
- * how to write and extend Application tests.
- * <p/>
- * To run this test, you can type:
- * adb shell am instrument -w \
- * -e class com.lovbomobile.android.locsy.test.activities.MainActivityTest \
- * com.lovbomobile.android.locsy.tests/android.test.InstrumentationTestRunner
- */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     MainActivity mainActivity;
--- src/com/lovbomobile/android/locsy/activities/LocsyApplication.java
+++ src/com/lovbomobile/android/locsy/activities/LocsyApplication.java
@@ -1,15 +1,30 @@
+/**
+ Copyright (c) 2013 Sven Schindler
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
 package com.lovbomobile.android.locsy.activities;
 
 import com.lovbomobile.android.locsy.client.LocsyClient;
 import com.lovbomobile.android.locsy.client.impl.RestLocsyClient;
 
-/**
- * Created with IntelliJ IDEA.
- * User: bongo
- * Date: 6/23/13
- * Time: 6:06 PM
- * To change this template use File | Settings | File Templates.
- */
 public class LocsyApplication {
 
     private static LocsyClient locsyClient;
